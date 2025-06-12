f6637bc968af2fb89a896dccbc6e08223c59b179
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-12 14:18:42 -0700

    update names

--- crates/next-api/src/project.rs
+++ crates/next-api/src/project.rs
@@ -1311,7 +1311,10 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("middleware-edge")),
+            LayerName::with_user_friendly_name(
+                rcstr!("middleware-edge"),
+                rcstr!("Edge Middleware"),
+            ),
         )))
     }
 
@@ -1365,7 +1368,7 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("middleware")),
+            LayerName::with_user_friendly_name(rcstr!("middleware"), rcstr!("Middleware")),
         )))
     }
 
